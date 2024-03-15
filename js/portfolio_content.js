ecb7d91907b900387eb583479a79bdea877220a9
Marlon Hernández Herrera <hndzmarlon@gmail.com>
2024-03-15 16:11:32 -0500

    Portfolio list behavior is implemented

--- js/portfolio_content.js
+++ js/portfolio_content.js
@@ -17,3 +17,26 @@ function loadPortfolioFooterSection(url) {
             .catch(error => console.log(error));
     });
 }
+
+// List behavior //
+let listElements = document.querySelectorAll('.list_title_father');
+
+listElements.forEach(listElement => {
+    listElement.addEventListener('click', () => {
+
+        listElement.classList.toggle('arrow')
+
+        let height = 0;
+        let childList = listElement.nextElementSibling; // toma al elemento siguiente
+
+        if (childList.clientHeight == "0") {
+            // height = childList.scrollHeight;
+            childList.querySelectorAll('li').forEach(li => {
+                height += li.offsetHeight;
+            });
+        }
+
+        childList.style.height = `${height}px`
+
+    })
+});
